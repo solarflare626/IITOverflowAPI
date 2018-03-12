e20f7a33f15bacef10d474b62b8812b9bbebed89
Roy Go Raposon Jr <roygoraposon@gmail.com>
2018-03-12 10:18:45 -0700

    added login /api/OAuthLogin

--- common/models/tag.js
+++ common/models/tag.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Tag) {
+	Tag.beforeCreate = function (next, model) {
+    model.createdAt = Date.now();
+    next();
+  };
+
+  Tag.beforeUpdate = function (next, model) {
+    model.updatedAt = Date.now();
+    next();
+  };
 
 };
