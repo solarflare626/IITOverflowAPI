9f0b6df2a87759c0e7adfa4a9350fd11df86cf89
Roy Go Raposon Jr <roygoraposonjr@gmail.com>
2018-04-02 13:45:16 +0800

    Added container for profile pics and remote method

--- server/boot/root.js
+++ server/boot/root.js
@@ -30,7 +30,23 @@ module.exports = function(app) {
   var QuestionFollow=app.models.QuestionFollow;
   var QuestionTag=app.models.QuestionTag;
   var Notification=app.models.Notification;
+  
+  app.models.container.createContainer({"name":"profile"}, function(err, fileObj) {
+            console.log("\nProfile container ok");
 
+  });
+  app.models.container.createContainer({"name":"question"}, function(err, fileObj) {
+            console.log("\nQuestion container ok");
+
+  });
+  app.models.container.createContainer({"name":"answer"}, function(err, fileObj) {
+            console.log("\nAnswer container ok");
+
+  });
+  app.models.container.createContainer({"name":"comment"}, function(err, fileObj) {
+            console.log("\nComment container ok");
+
+  });
 
   var lbTables = ['Notification','user', 'Follow', 'Category', 'Tag','Question','CustomToken','Answer','Comment','AnswerDownvote','AnswerUpvote','Conversation','Expert','Interest','Message','QuestionDownvote','QuestionUpvote','QuestionFollow','QuestionTag'];
   mysqlDs.autoupdate(lbTables, function(er) {
